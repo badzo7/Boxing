31d5ad992b2475a472cccc4c5e5a4a4cb940ac56
badzo7 <canva2023compte@gmail.com>
2025-05-27 12:55:43 +0400

    Update CustomizerPage with size selector

--- src/components/customizer/SizeSelector.tsx
+++ src/components/customizer/SizeSelector.tsx
@@ -38,7 +38,7 @@ const SizeSelector: React.FC = () => {
                 : 'border-neutral-500'
             }`}>
               {glove.size === item.size && (
-                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round">
+                <svg xmlns="http://www.w3.org/2000/svg\" width="14\" height="14\" viewBox="0 0 24 24\" fill="none\" stroke="currentColor\" strokeWidth="3\" strokeLinecap="round\" strokeLinejoin="round">
                   <polyline points="20 6 9 17 4 12"></polyline>
                 </svg>
               )}
